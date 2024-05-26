b723bfe8f0b8d1c67dad0c0b3ae1270de16c77c4
AryaEm <ary4maulanaa@gmail.com>
2024-05-27 06:26:59 +0700

    ganti bg

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-const menuicon = document.getElementById('menu-icon'); //
-const menulist = document.getElementById('menu-list');
+    const menuicon = document.getElementById('menu-icon'); //
+    const menulist = document.getElementById('menu-list');
 
-menuicon.addEventListener('click', () => {
-    menulist.classList.toggle('hidden');
-    console.log(menuicon.classList)
-});
+    menuicon.addEventListener('click', () => {
+        menulist.classList.toggle('hidden');
+        console.log(menuicon.classList)
+    });
